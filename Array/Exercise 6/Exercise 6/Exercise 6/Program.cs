92131defde1c4e3e2c7a1c30068077592029f16d
Nikita Tumashevich <ntumasevic@gmail.com>
2021-11-28 23:00:36 +0300

    /

--- Array/Exercise 6/Exercise 6/Exercise 6/Program.cs
+++ Array/Exercise 6/Exercise 6/Exercise 6/Program.cs
@@ -6,7 +6,39 @@ namespace Exercise_6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Задан массив из n целых чисел. Вывести только его положительные элементы, не меняя первоначальный порядок.
+
+            while (true)
+            {
+                Console.Clear();
+
+                Console.Write("Введите количество элементов в массиве: ");
+
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                double[] a = new double[n];  // массив
+
+                // Создание массива
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    Console.Write($"Введите {i} элемент: ");
+
+                    a[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                // Вывод массива на экран
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[i] > 0)
+                    {
+                        Console.Write($"{a[i]} ");
+                    }
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
