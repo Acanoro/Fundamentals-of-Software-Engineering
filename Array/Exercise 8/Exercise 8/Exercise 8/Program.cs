0396851bd62ab4b3acce50b01af6e5c96aed3fb0
Nikita Tumashevich <ntumasevic@gmail.com>
2021-11-30 18:56:09 +0300

    /

--- Array/Exercise 8/Exercise 8/Exercise 8/Program.cs
+++ Array/Exercise 8/Exercise 8/Exercise 8/Program.cs
@@ -18,7 +18,11 @@ namespace Exercise_8
 
                 double[] a = new double[n];  // 1 массив
 
+                double the_sum_of_all_numbers = 0;
 
+                double sum_of_numbers = 0;
+
+                int amount_of_numbers = 0;
 
                 // Создание массива
 
@@ -29,12 +33,24 @@ namespace Exercise_8
                     a[i] = Convert.ToInt32(Console.ReadLine());
                 }
 
-                // Вывод массива на экран
+                // Hаходит сумму всех чисел
+
+                for (int i = 0; i < a.Length; i++)
+                    the_sum_of_all_numbers += a[i];
+
+                // Hаходит сумму и количество чисел, больших среднего арифметического элементов массива
 
                 for (int i = 0; i < a.Length; i++)
                 {
-                    Console.Write($"{a[i]}\n");
+                    if (a[i] > the_sum_of_all_numbers / a.Length)
+                    {
+                        sum_of_numbers += a[i];
+
+                        amount_of_numbers += 1;
+                    }
                 }
+
+                Console.WriteLine($"Сумма больших среднего арифметического элементов массива: {sum_of_numbers}\nКоличество чисел больших среднего арифметического элементов массива: {amount_of_numbers}");
 
                 Console.ReadLine();
             }
